e39bef4f53e6e7c7b5341daddc5afcc7e4865139
Marolski <mbrzoska303@gmail.com>
2023-01-23 18:56:33 +0100

    nowa struktura tabel SQL

--- school.sql
+++ school.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 13 Sty 2023, 17:36
+-- Czas generowania: 23 Sty 2023, 18:55
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.2.0
 
@@ -39,8 +39,7 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`courseId`, `courseName`, `courseTeacherId`, `courseGroupId`) VALUES
-(1, 'CH_course', 1, 1),
-(2, 'PL_course', 2, 2);
+(1, 'BG_course', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -62,17 +61,12 @@ CREATE TABLE `grades` (
 --
 
 INSERT INTO `grades` (`gradeId`, `grade`, `gradeStudentId`, `gradeComment`, `gradeCourseId`, `gradeTimestamp`) VALUES
-(1, 1, 1, 'Suscipit nam cumque.', 1, '2023-01-13'),
-(2, 6, 1, 'Molestiae assumenda.', 1, '2023-01-13'),
-(3, 6, 1, 'Ut eum sed.', 1, '2023-01-13'),
-(4, 6, 1, 'Molestias occaecati.', 1, '2023-01-13'),
-(5, 3, 1, 'Iure ut culpa quis.', 1, '2023-01-13'),
-(6, 1, 2, 'Cumque est enim.', 2, '2023-01-13'),
-(7, 1, 3, 'Quasi rerum.', 2, '2023-01-13'),
-(8, 5, 4, 'Possimus ullam et.', 2, '2023-01-13'),
-(9, 1, 5, 'Facilis est.', 2, '2023-01-13'),
-(10, 4, 6, 'Incidunt autem.', 2, '2023-01-13'),
-(11, 1, 7, 'Ipsum eos sit nisi.', 2, '2023-01-13');
+(1, 3, 1, 'Placeat aut.', 1, '2023-01-23'),
+(2, 1, 2, 'Aut tempore nihil.', 1, '2023-01-23'),
+(3, 5, 3, 'Quo tempora est.', 1, '2023-01-23'),
+(4, 1, 4, 'Eaque voluptates.', 1, '2023-01-23'),
+(5, 6, 5, 'Quas saepe neque.', 1, '2023-01-23'),
+(6, 4, 6, 'Officia ut.', 1, '2023-01-23');
 
 -- --------------------------------------------------------
 
@@ -90,8 +84,7 @@ CREATE TABLE `groups` (
 --
 
 INSERT INTO `groups` (`groupId`, `groupName`) VALUES
-(1, 'Luxembourg_grupa'),
-(2, 'Belgium_grupa');
+(1, 'Cape Verde_grupa');
 
 -- --------------------------------------------------------
 
@@ -112,13 +105,12 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`ID`, `Name`, `Surname`, `Password`, `studentGroupId`) VALUES
-(1, 'Benny', 'Weber', 'szkola1', 1),
-(2, 'Athena', 'Ward', 'szkola1', 2),
-(3, 'Florence', 'Brown', 'szkola1', 2),
-(4, 'Carey', 'Ward', 'szkola1', 2),
-(5, 'Drake', 'Eichmann', 'szkola1', 2),
-(6, 'Aric', 'Lind', 'szkola1', 2),
-(7, 'Rasheed', 'Kuhic', 'szkola1', 2);
+(1, 'Dedric', 'Mertz', 'szkola1', 1),
+(2, 'Ahmad', 'Daugherty', 'szkola1', 1),
+(3, 'Wellington', 'Baumbach', 'szkola1', 1),
+(4, 'Jamie', 'Kerluke', 'szkola1', 1),
+(5, 'Trevor', 'Hirthe', 'szkola1', 1),
+(6, 'Madge', 'Hirthe', 'szkola1', 1);
 
 -- --------------------------------------------------------
 
@@ -138,17 +130,12 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`taskId`, `taskDescription`, `taskCourseId`, `taskTimestamp`) VALUES
-(1, 'Eaque illo et.', 1, '2023-01-13'),
-(2, 'Nemo magnam sunt.', 1, '2023-01-13'),
-(3, 'Dicta adipisci.', 1, '2023-01-13'),
-(4, 'Numquam quas itaque.', 1, '2023-01-13'),
-(5, 'Fuga nobis esse aut.', 1, '2023-01-13'),
-(6, 'Neque dignissimos.', 2, '2023-01-13'),
-(7, 'Neque aut sed ab.', 2, '2023-01-13'),
-(8, 'Et voluptates qui.', 2, '2023-01-13'),
-(9, 'At a aut dolores.', 2, '2023-01-13'),
-(10, 'Et voluptas omnis.', 2, '2023-01-13'),
-(11, 'Nulla et pariatur.', 2, '2023-01-13');
+(1, 'Enim voluptatum.', 1, '2023-01-23'),
+(2, 'Aut suscipit.', 1, '2023-01-23'),
+(3, 'Et dolore nihil.', 1, '2023-01-23'),
+(4, 'Veniam consectetur.', 1, '2023-01-23'),
+(5, 'Incidunt omnis.', 1, '2023-01-23'),
+(6, 'Ut ducimus sequi.', 1, '2023-01-23');
 
 -- --------------------------------------------------------
 
@@ -168,8 +155,7 @@ CREATE TABLE `teachers` (
 --
 
 INSERT INTO `teachers` (`ID`, `Name`, `Surname`, `Password`) VALUES
-(1, 'Lola', 'Pfeffer', 'B^a{$M#'),
-(2, 'Andres', 'Mills', '7)OHl$b{{?');
+(1, 'Jaquelin', 'Marquardt', '5(3#f:(O@');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -225,37 +211,37 @@ ALTER TABLE `teachers`
 -- AUTO_INCREMENT dla tabeli `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `courseId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `courseId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT dla tabeli `grades`
 --
 ALTER TABLE `grades`
-  MODIFY `gradeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `gradeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT dla tabeli `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `groupId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `groupId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT dla tabeli `students`
 --
 ALTER TABLE `students`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT dla tabeli `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `taskId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `taskId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT dla tabeli `teachers`
 --
 ALTER TABLE `teachers`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Ograniczenia dla zrzutów tabel
